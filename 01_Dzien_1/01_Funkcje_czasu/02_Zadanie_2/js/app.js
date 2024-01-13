03633088a50fb5e3203d39d4b76c71746bd82ffe
paulinka <paulina.lenar22@gmail.com>
2024-01-13 11:16:24 +0100

    time functions

--- 01_Dzien_1/01_Funkcje_czasu/02_Zadanie_2/js/app.js
+++ 01_Dzien_1/01_Funkcje_czasu/02_Zadanie_2/js/app.js
@@ -1,1 +1,29 @@
+function countHello(n) {
+    let counter = 0;
+    const intervalId = setInterval(function () {
+        counter++
+        if (counter >= n) {
+            clearInterval(intervalId)
+        }
+        console.log("Hello")
+        
+    }, 1000)
+}
 
+countHello(5)
+
+function chlopi(){
+    const chlopiZmienna = "chłopi";
+    
+    function szlachta(){
+        const szlachtaZmienna = "szlachta";
+        console.log("======SZLACHTA========")
+        console.log(szlachtaZmienna)
+        console.log(chlopiZmienna);
+    }
+}
+function sayHello(company){
+    return function (name){
+        console.log('Cześć')
+    }
+}
