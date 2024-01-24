3139f92647b330783fc2b51f292b8df4e7884990
paulinka <paulina.lenar22@gmail.com>
2024-01-24 13:22:12 +0100

    day 1 homework nr 3

---
+++ 02_Dzien_1_-_Praca_domowa/02_Zadanie_2/js/app.js
@@ -0,0 +1,6 @@
+// addTheSameNumbers(7, [4, 7, 24, 7, 0, 10]) => 14
+// addTheSameNumbers(9, [33, 54, 2, 1, 9, 100] ) => 9
+// addTheSameNumbers(0, [33, 0, 2, 0, 9, 0] ) => 0
+// addTheSameNumbers(43, [6, 0, 2, 2, 3, 46] ) => null
+
+
--- 02_Dzien_1_-_Praca_domowa/03_Zadanie_3/js/app.js
+++ 02_Dzien_1_-_Praca_domowa/03_Zadanie_3/js/app.js
@@ -1,34 +1,44 @@
 const planets = [
-  {
-    name: "Merkury",
-    numberOfMoons: 0
-  },
-  {
-    name: "Wenus",
-    numberOfMoons: 0
-  },
-  {
-    name: "Ziemia",
-    numberOfMoons: 1
-  },
-  {
-    name: "Mars",
-    numberOfMoons: 2
-  },
-  {
-    name: "Jowisz",
-    numberOfMoons: 63
-  },
-  {
-    name: "Saturn",
-    numberOfMoons: 60
-  },
-  {
-    name: "Uran",
-    numberOfMoons: 27
-  },
-  {
-    name: "Neptun",
-    numberOfMoons: 13
-  }
+    {
+        name: "Merkury",
+        numberOfMoons: 0
+    },
+    {
+        name: "Wenus",
+        numberOfMoons: 0
+    },
+    {
+        name: "Ziemia",
+        numberOfMoons: 1
+    },
+    {
+        name: "Mars",
+        numberOfMoons: 2
+    },
+    {
+        name: "Jowisz",
+        numberOfMoons: 63
+    },
+    {
+        name: "Saturn",
+        numberOfMoons: 60
+    },
+    {
+        name: "Uran",
+        numberOfMoons: 27
+    },
+    {
+        name: "Neptun",
+        numberOfMoons: 13
+    }
 ];
+planets.sort();
+console.log(planets);
+
+function compareNumbers(a, b) {
+  return a - b;
+}
+planets.sort((name, numberOfMoons) => name - numberOfMoons);
+
+sorted[0] = 63;
+console.log(numbers[0]);
