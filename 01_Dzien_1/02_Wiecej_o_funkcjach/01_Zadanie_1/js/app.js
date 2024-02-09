585475e1f95042415a984fc14202831b66804f27
paulinka <paulina.lenar22@gmail.com>
2024-02-09 15:08:44 +0100

    day 4 zadanie 4

--- 01_Dzien_1/02_Wiecej_o_funkcjach/01_Zadanie_1/js/app.js
+++ 01_Dzien_1/02_Wiecej_o_funkcjach/01_Zadanie_1/js/app.js
@@ -1,15 +1,32 @@
+// 3 wersje pisania funkcji
+// function add(value1, value2){
+//  wersja 1
+
+// function add(value1,value2){
+// return value1,value2;
+
+//wersja 2
+
+// const add=function(value1,value2){
+// return value1,value2;
+
+//wersja 3
+// const add=(value1,value2) => {
+// return value1,value2;
+
+
 function firstFunc() {
-  const someInt = 1;
+    const someInt = 1;
 
-  function secondFunc() {
-    console.log(someInt);
+    function secondFunc() {
+        console.log(someInt);
 
-    const otherInt = 3;
-  }
+        const otherInt = 3;
+    }
 
-  secondFunc();
-  // nie ma dostępu ponieważ funkcja otherInt jest funkcja lokalna zagniezdzona w funkcji globalnej
-  console.log(otherInt);
+    secondFunc();
+    // nie ma dostępu ponieważ funkcja otherInt jest funkcja lokalna zagniezdzona w funkcji globalnej
+    console.log(otherInt);
 }
 
 firstFunc();
